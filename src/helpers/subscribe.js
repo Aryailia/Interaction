ed4867b9ffa66d9f3c224efe8169e7412f63e375
Aryailia <Aryailia@users.noreply.github.com>
2017-06-29 12:48:45 +0800

    subscribe initial working code

--- src/helpers/subscribe.js
+++ src/helpers/subscribe.js
@@ -2,20 +2,40 @@ const eventTypeList = {
   //mousemove: WeakSet(document, [])
 };
 
-function handler(ev) {
-
+function emitter(ev) {
+  const handlerList = eventTypeList[ev.type].get(ev.currentTarget);
+  for (let fn of handlerList) {
+    fn(ev);
+  }
 }
 
 const EventStream = {
-  subscribe: function (source, eventName) {
+  subscribe: function (source, eventName, fn) {
     if (!eventTypeList.hasOwnProperty(eventName)) {
       eventTypeList[eventName] = new WeakMap();
     }
     
-    if (!eventTypeList[eventName].has(source)) {
-      source.addEventListener(eventName, handler);
-      eventTypeList[eventName].set(source, []);
+    const uniqueId = Symbol();
+    const eventHandlerMap = eventTypeList[eventName];
+    if (!eventHandlerMap.has(source)) {
+      source.addEventListener(eventName, emitter);
+      eventHandlerMap.set(source, []);
     }
+
+    const handlerList = eventHandlerMap.get(source);
+    handlerList.push({ key: uniqueId, value: fn });
+
+    return function unsubscribe() {
+      const length = handlerList.length;
+      let index = -1;
+      while (++index < length) {
+        if (handlerList[index].key === uniqueId) {
+          handlerList.splice(index, 1);
+          break;
+        }
+        // Not sure 
+      }
+    };
   },
 
   throttle: function () {
